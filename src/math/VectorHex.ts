fb90553b1e3b2e2d25b0dee1ecdb1269c2a725f4
DrK-ode <mailtokaj@gmail.com>
2025-01-17 08:14:08 +0100

    More tests and more fixes.

--- src/math/VectorHex.ts
+++ src/math/VectorHex.ts
@@ -1,6 +1,6 @@
 import { Vector2D, Vector2DLike } from './Vector2D';
 
-const sqrt3over2: number = 0.8660254038;
+const sqrt3over2: number = 0.5 * Math.sqrt(3);
 
 /**
  * @property {Horizontal} - Hexes have horizontal top and bottom edges and the q-axis is aligned with x-axis.
@@ -11,28 +11,34 @@ export enum HexGridOrientation {
     Vertical
 }
 
-export enum HorizontalOrientationAxis {
+enum HorizontalOrientationAxis {
     East = 0,
     NorthWest = 1,
     SouthWest = 2,
 }
 
-export enum HorizontalOrientationDirection {
+export enum HorizontalOrientationEdge {
     North = 0,
-    SouthWest = 1,
-    SouthEast = 2,
+    NorthWest = 1,
+    SouthWest = 2,
+    South = 3,
+    SouthEast = 4,
+    NorthEast = 5
 }
 
-export enum VerticalOrientationAxis {
+enum VerticalOrientationAxis {
     NorthEast = 0,
-    West = 1,
-    SouthEast = 2
+    NorthWest = 1,
+    South = 2
 }
 
-export enum VerticalOrientationDirection {
+export enum VerticalOrientationEdge {
     NorthWest = 0,
-    SouthWest = 1,
-    East = 2
+    West = 1,
+    SouthWest = 2,
+    SouthEast = 3,
+    East = 4,
+    NorthEast = 5
 }
 
 /**
@@ -54,7 +60,7 @@ export class VectorHex {
      * @param {number} [q=0] - The first hex coordinate.
      * @param {number} [r=0] - The second hex coordinate.
      * @returns {VectorHex} - A new @this.
-     * @throws if the coordinates are not integers.
+     * @throws if the coordinates are not integral.
      */
     public constructor(q: number = 0, r: number = 0) {
         if (!(Number.isInteger(q) && Number.isInteger(r))) {
@@ -83,6 +89,7 @@ export class VectorHex {
      * @param {HexGridOrientation} orientation - Determines the hex grid axis alignment.
      * @param {number=1} [size=1] - The distance between two adjacent hexes.
      * @returns {Vector2D} - A vector pointing from the origin to the center of the hex.
+     * @throws if the size is not positive.
      */
     public into_vector2(orientation: HexGridOrientation = HexGridOrientation.Horizontal, size: number = 1): Vector2D {
         if (size <= 0) {
@@ -96,25 +103,18 @@ export class VectorHex {
         }
     }
 
-    /** 
-     * @returns {number} - The number of steps, from the origin, needed to reach the hex pointed to by this.
-     */
-    public manhattan(): number {
-        return Math.max(Math.abs(this.q), Math.abs(this.r), Math.abs(this.s()));
-    }
-
     /**
-     * Vector subtraction.
+     * Calculates vector pointing from one hex to another.
      * @param {@this} [vec1] - Starting hex.
      * @param {@this} [vec2] - Ending hex.
      * @returns {@this} - A vector pointing from hex1 to hex2
      */
-    public static difference(vec1: VectorHex, vec2: VectorHex): VectorHex {
+    public static delta(vec1: VectorHex, vec2: VectorHex): VectorHex {
         return new VectorHex(vec2.q - vec1.q, vec2.r - vec1.r);
     }
 
     /**
-     * Subtracts a vector.
+     * Subtracts a vector from this one.
      * @param {@this} [hex] - The vector to subtract 
      * @returns {@this} - The vector after subtraction.
      */
@@ -125,12 +125,12 @@ export class VectorHex {
     }
 
     /**
-     * Vector addition.
+     * Vector sum.
      * @param {@this} [origin] - Starting hex.
      * @param {@this} [translation] - Translation to apply.
      * @returns {@this} - The sum of the vectors.
      */
-    public static add(origin: VectorHex, translation: VectorHex): VectorHex {
+    public static sum(origin: VectorHex, translation: VectorHex): VectorHex {
         return new VectorHex(origin.q + translation.q, origin.r + translation.r);
     }
 
@@ -145,19 +145,26 @@ export class VectorHex {
         return this;
     }
 
+    /** 
+     * @returns {number} - The number of steps, from the origin, needed to reach the hex pointed to by this.
+     */
+    public manhattan(): number {
+        return Math.max(Math.abs(this.q), Math.abs(this.r), Math.abs(this.s()));
+    }
+
     /**
      * Which sector does the vector point to?
-     * @returns {number} - An index identifying the sector. Indices start at 0 indicating the east sector (horizontal orientation) or the north-east sector (vertical orientation).
+     * @returns {number} - An index >= 0 and < 6, identifying the sector. Indices start at 0 indicating the east sector (horizontal orientation) or the north-east sector (vertical orientation).
      */
     public sector(): number {
         if (Math.abs(this.q) >= Math.abs(this.r) && Math.abs(this.q) > Math.abs(this.s())) {
             return this.q >= 0 ? 0 : 3
         }
         else if (Math.abs(this.r) >= Math.abs(this.s())) {
-            return this.r >= 0 ? 1 : 4
+            return this.r > 0 ? 2 : 5
         }
         else {
-            return this.s() >= 0 ? 2 : 5;
+            return this.s() < 0 ? 1 : 4;
         }
     }
 
@@ -167,84 +174,97 @@ export class VectorHex {
      * @param steps 
      * @throws if an invalid axis number is found.
      */
-    public step_in_direction(direction: number, steps: number = 1) {
-        if (direction < 0 || direction >= 3) {
-            throw new Error("Invalid direction, must be a positive integer < 3.");
+    public step_in_direction(direction: number, steps: number = 1): VectorHex {
+        if (!Number.isInteger(steps)) {
+            throw new Error('Number of steps must be an integer.');
+        }
+        if (direction < 0 || direction >= 6) {
+            throw new Error("Invalid direction, must be a positive integer < 6.");
+        }
+        if (direction > 2) {
+            steps *= -1;
+            direction -= 3;
         }
         switch (direction) {
             // North / NorthWest
             case 0:
                 this.r += steps;
                 break;
-            // SouthWest
-            case 1:
+            case 1: // NorthWest / West
+                this.q -= steps;
+                this.r += steps;
+                break;
+            case 2: // SouthWest 
                 this.q -= steps;
                 break;
-            // SouthEast / East
-            case 2:
-                this.q += steps;
-                this.r -= steps;
-                break;
         }
-    }
-
-    /**
-     * Rotates the vector n hexes counter clockwise.
-     * @param {number} [n=1] - The number of clockwise steps (can be negative).
-     */
-    public step_rotation(n: number) {
-        if (n == 0) return;
-        const hexes_per_sector = this.manhattan();
-        const circumference = 6 * hexes_per_sector;
-        n %= circumference;
-        n = n < 0 ? n + circumference : n; // 0 <= n < circumference
-
-        n -= this.rotate_to_corner();
-        const sector_rotations = (n / hexes_per_sector);
-        this.sector_rotation(sector_rotations);
-        n -= sector_rotations * hexes_per_sector;
-        let sector = this.sector();
-        let direction = sector % 3;
-        let steps = sector < 3 ? n : -n;
-        this.step_in_direction(steps, direction);
-    }
-
-    private rotate_to_corner(): number {
-        // Rotate one hex at a time until a corner is reached
-        let current_sector: number = this.sector();
-        let steps_taken: number = 0;
-        switch (current_sector) {
-            case 0:
-            case 3:
-                steps_taken = Math.abs(this.r);
-                this.r = 0;
-                break;
-            case 1:
-            case 4:
-                steps_taken = Math.abs(this.q);
-                this.r += this.q;
-                this.q = 0;
-                break;
-            case 2:
-            case 5:
-                steps_taken = Math.abs(this.s());
-                this.q += this.s();
-        }
-        return steps_taken;
+        return this;
     }
 
     /**
      * Rotates the vector n times 60 degrees counter clockwise.
      * @param {number} [n] - The number of 60 degree rotations (can be negative).
+     * @throws if the number of sector rotations is not integral.
      */
     public sector_rotation(n: number) {
-        if (n == 0) return;
+        if (!Number.isInteger(n)) {
+            throw new Error('Number of sectors must be integral.');
+        }
         n %= 6;
+        if (n == 0) return;
         n = n < 0 ? n + 6 : n;
         const sign_shift = 1 - (n % 2) * 2;
         const coordinate_shift = n % 3;
         const coordinates: number[] = [this.q, this.r, this.s()];
         this.q = sign_shift * coordinates[coordinate_shift];
-        this.r = sign_shift * coordinates[1 + coordinate_shift];
+        this.r = sign_shift * coordinates[(1 + coordinate_shift) % 3];
+    }
+
+    /**
+     * Rotates the vector n hexes counter clockwise.
+     * @param {number} [n] - The number of counter clockwise steps (can be negative).
+     * @throws if the number of steps is not integral.
+     */
+    public step_rotation(n: number) {
+        if (!Number.isInteger(n)) {
+            throw new Error('Number of steps must be integral.')
+        }
+        if (n == 0) return;
+        const hexes_per_sector = this.manhattan();
+        if (hexes_per_sector == 0) return;
+        const circumference = 6 * hexes_per_sector;
+        n %= circumference;
+        n = n < 0 ? n + circumference : n; // 0 <= n < circumference
+        n -= this.rotate_to_corner(n);
+        const sector_rotations = Math.trunc(n / hexes_per_sector);
+        this.sector_rotation(sector_rotations);
+        n -= sector_rotations * hexes_per_sector;
+        let sector = this.sector();
+        let direction = sector;
+        this.step_in_direction(direction, n);
+    }
+
+    private rotate_to_corner(max_steps: number): number {
+        // Rotate hex wise until a corner is reached
+        let current_sector: number = this.sector();
+        let steps_taken: number = 0;
+        switch (current_sector) {
+            case 0:
+            case 3:
+                steps_taken = Math.min(Math.abs(this.r), max_steps);
+                this.step_in_direction(current_sector, steps_taken);
+                break;
+            case 1:
+            case 4:
+                steps_taken = Math.min(Math.abs(this.q), max_steps);
+                this.step_in_direction(current_sector, steps_taken);
+                break;
+
+            case 2:
+            case 5:
+                steps_taken = Math.min(Math.abs(this.s()), max_steps);
+                this.step_in_direction(current_sector, steps_taken);
+        }
+        return steps_taken;
     }
 }
