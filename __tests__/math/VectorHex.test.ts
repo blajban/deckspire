d9330dfac0e01e9bd2054f2cb802a4b75f650562
DrK-ode <mailtokaj@gmail.com>
2025-01-18 21:39:46 +0100

    Working transformation between Vector2D and VectorHex.

--- __tests__/math/VectorHex.test.ts
+++ __tests__/math/VectorHex.test.ts
@@ -1,9 +1,4 @@
-import {
-  HexGridOrientation,
-  HorizontalOrientationEdge,
-  VectorHex,
-  VerticalOrientationEdge,
-} from '../../src/math/VectorHex';
+import { HexLayout, HexDirection, VectorHex } from '../../src/math/VectorHex';
 
 const FLOAT_DIGITS: number = 8;
 
@@ -41,17 +36,38 @@ describe('Creating VectorHex', () => {
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(-1);
     vec = VectorHex.from_vector2D({
-      x: (1.001 * Math.cos(Math.PI / 6)) / Math.sqrt(2),
-      y: (1.001 * Math.sin(Math.PI / 6)) / Math.sqrt(2),
-    });
-    expect(vec.q).toBe(1);
-    expect(vec.r).toBe(0);
-    vec = VectorHex.from_vector2D({
-      x: (0.999 * Math.cos(Math.PI / 6)) / Math.sqrt(2),
-      y: (0.999 * Math.sin(Math.PI / 6)) / Math.sqrt(2),
+      x: (1 / Math.sqrt(3)) * Math.cos(Math.PI / 3) - 0.001,
+      y: (1 / Math.sqrt(3)) * Math.sin(Math.PI / 3) + 0.001,
     });
     expect(vec.q).toBe(0);
-    expect(vec.r).toBe(0);
+    expect(vec.r).toBe(1);
+    vec = VectorHex.from_vector2D({
+      x: 1.001 * 0.5 * Math.cos(Math.PI / 6),
+      y: 1.001 * 0.5 * Math.sin(Math.PI / 6),
+    });
+    expect(vec.q === 1).toBeTruthy();
+    expect(vec.r === 0).toBeTruthy();
+    vec = VectorHex.from_vector2D({
+      x: 0.999 * 0.5 * Math.cos(Math.PI / 6),
+      y: 0.999 * 0.5 * Math.sin(Math.PI / 6),
+    });
+    expect(vec.q === 0).toBeTruthy();
+    expect(vec.r === 0).toBeTruthy();
+    vec = VectorHex.from_vector2D({
+      x: 2 * Math.cos(Math.PI / 3),
+      y: 2 * Math.sin(Math.PI / 3),
+    });
+    expect(vec.q === 1).toBeTruthy();
+    expect(vec.r === 1).toBeTruthy();
+    vec = VectorHex.from_vector2D(
+      {
+        x: 2 * Math.cos(Math.PI / 3),
+        y: 2 * Math.sin(Math.PI / 3),
+      },
+      HexLayout.Vertical,
+    );
+    expect(vec.q === 2).toBeTruthy();
+    expect(vec.r === 0).toBeTruthy();
   });
   test('From clone', () => {
     const vec = new VectorHex(3, 7);
@@ -64,76 +80,76 @@ describe('Creating VectorHex', () => {
 describe('Converting VectorHex', () => {
   test('Horizontal hex into Vector2D', () => {
     let vec;
-    vec = new VectorHex(0, 0).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(0, 0).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0, FLOAT_DIGITS);
-    vec = new VectorHex(1, 0).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(1, 0).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0.5 * Math.sqrt(3), FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0.5, FLOAT_DIGITS);
-    vec = new VectorHex(0, 1).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(0, 1).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(1, FLOAT_DIGITS);
-    vec = new VectorHex(-1, 1).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(-1, 1).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(-0.5 * Math.sqrt(3), FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0.5, FLOAT_DIGITS);
-    vec = new VectorHex(-1, 0).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(-1, 0).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(-0.5 * Math.sqrt(3), FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(-0.5, FLOAT_DIGITS);
-    vec = new VectorHex(0, -1).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(0, -1).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(-1, FLOAT_DIGITS);
-    vec = new VectorHex(1, -1).into_vector2d(HexGridOrientation.Horizontal, 1);
+    vec = new VectorHex(1, -1).into_vector2d(HexLayout.Horizontal, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0.5 * Math.sqrt(3), FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(-0.5, FLOAT_DIGITS);
   });
   test('Vertical hex into Vector2D', () => {
     let vec;
-    vec = new VectorHex(0, 0).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(0, 0).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0, FLOAT_DIGITS);
-    vec = new VectorHex(1, 0).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(1, 0).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0.5, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0.5 * Math.sqrt(3), FLOAT_DIGITS);
-    vec = new VectorHex(0, 1).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(0, 1).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(-0.5, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0.5 * Math.sqrt(3), FLOAT_DIGITS);
-    vec = new VectorHex(-1, 1).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(-1, 1).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(-1, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0, FLOAT_DIGITS);
-    vec = new VectorHex(-1, 0).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(-1, 0).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(-0.5, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(-0.5 * Math.sqrt(3), FLOAT_DIGITS);
-    vec = new VectorHex(0, -1).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(0, -1).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(0.5, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(-0.5 * Math.sqrt(3), FLOAT_DIGITS);
-    vec = new VectorHex(1, -1).into_vector2d(HexGridOrientation.Vertical, 1);
+    vec = new VectorHex(1, -1).into_vector2d(HexLayout.Vertical, 1);
     expect(vec.length()).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(0, FLOAT_DIGITS);
   });
   test('Hex to Vector2D with scaling', () => {
     expect(() =>
-      new VectorHex(1, 0).into_vector2d(HexGridOrientation.Horizontal, 0),
+      new VectorHex(1, 0).into_vector2d(HexLayout.Horizontal, 0),
     ).toThrow('Hex size must be positive.');
     let vec;
-    vec = new VectorHex(1, 0).into_vector2d(HexGridOrientation.Horizontal, 2);
+    vec = new VectorHex(1, 0).into_vector2d(HexLayout.Horizontal, 2);
     expect(vec.length()).toBeCloseTo(2, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(Math.sqrt(3), FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(1, FLOAT_DIGITS);
-    vec = new VectorHex(1, 0).into_vector2d(HexGridOrientation.Vertical, 2);
+    vec = new VectorHex(1, 0).into_vector2d(HexLayout.Vertical, 2);
     expect(vec.length()).toBeCloseTo(2, FLOAT_DIGITS);
     expect(vec.x).toBeCloseTo(1, FLOAT_DIGITS);
     expect(vec.y).toBeCloseTo(Math.sqrt(3), FLOAT_DIGITS);
@@ -211,96 +227,136 @@ describe('Hex grid operations and calculations.', () => {
     const hex2 = new VectorHex(-2, 1);
     expect(VectorHex.delta(hex1, hex2).manhattan()).toBe(5);
   });
-  test('Sector', () => {
-    expect(new VectorHex(1, -1).sector()).toBe(0);
-    expect(new VectorHex(1, 0).sector()).toBe(1);
-    expect(new VectorHex(0, 1).sector()).toBe(2);
-    expect(new VectorHex(-1, 1).sector()).toBe(3);
-    expect(new VectorHex(-1, 0).sector()).toBe(4);
-    expect(new VectorHex(0, -1).sector()).toBe(5);
+  test('Hex wedge', () => {
+    expect(new VectorHex(1, -1).wedge()).toBe(HexDirection.East);
+    expect(new VectorHex(1, 0).wedge()).toBe(HexDirection.NorthEast);
+    expect(new VectorHex(0, 1).wedge()).toBe(HexDirection.NorthWest);
+    expect(new VectorHex(-1, 1).wedge()).toBe(HexDirection.West);
+    expect(new VectorHex(-1, 0).wedge()).toBe(HexDirection.SouthWest);
+    expect(new VectorHex(0, -1).wedge()).toBe(HexDirection.SouthEast);
+    expect(new VectorHex(1, -1).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.NorthEast,
+    );
+    expect(new VectorHex(1, 0).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.North,
+    );
+    expect(new VectorHex(0, 1).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.NorthWest,
+    );
+    expect(new VectorHex(-1, 1).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.SouthWest,
+    );
+    expect(new VectorHex(-1, 0).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.South,
+    );
+    expect(new VectorHex(0, -1).wedge(HexLayout.Vertical)).toBe(
+      HexDirection.SouthEast,
+    );
   });
   test('Single steps in horizontal orientation directions', () => {
+    expect(() =>
+      new VectorHex(0, 0).step_in_direction(HexDirection.North, 0.5),
+    ).toThrow('must be integral');
+    expect(() =>
+      new VectorHex(0, 0).step_in_direction(HexDirection.East),
+    ).toThrow('Invalid');
+    expect(() =>
+      new VectorHex(0, 0).step_in_direction(
+        HexDirection.North,
+        1,
+        HexLayout.Vertical,
+      ),
+    ).toThrow('Invalid');
     const vec = new VectorHex(0, 0);
-    vec.step_in_direction(HorizontalOrientationEdge.North, 1);
+    vec.step_in_direction(HexDirection.North, 1);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(1);
-    vec.step_in_direction(HorizontalOrientationEdge.SouthEast, 1);
+    vec.step_in_direction(HexDirection.SouthEast, 1);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(0);
-    vec.step_in_direction(HorizontalOrientationEdge.SouthWest, 1);
+    vec.step_in_direction(HexDirection.SouthWest, 1);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(0);
-    vec.step_in_direction(HorizontalOrientationEdge.SouthEast, -1);
+    vec.step_in_direction(HexDirection.SouthEast, -1);
     expect(vec.q).toBe(-1);
     expect(vec.r).toBe(1);
-    vec.step_in_direction(HorizontalOrientationEdge.SouthWest, -1);
-    expect(vec.q).toBe(0);
-    expect(vec.r).toBe(1);
-    vec.step_in_direction(HorizontalOrientationEdge.North, -1);
+    vec.step_in_direction(HexDirection.North, -1);
+    expect(vec.q).toBe(-1);
+    expect(vec.r).toBe(0);
+    vec.step_in_direction(HexDirection.SouthWest, -1);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(0);
   });
   test('Single steps in vertical orientation directions', () => {
     const vec = new VectorHex(0, 0);
-    vec.step_in_direction(VerticalOrientationEdge.East, 1);
+    vec.step_in_direction(HexDirection.East, 1, HexLayout.Vertical);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(-1);
-    vec.step_in_direction(VerticalOrientationEdge.NorthWest, 1);
+    vec.step_in_direction(HexDirection.NorthWest, 1, HexLayout.Vertical);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(0);
-    vec.step_in_direction(VerticalOrientationEdge.SouthWest, 1);
+    vec.step_in_direction(HexDirection.SouthWest, 1, HexLayout.Vertical);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(0);
-    vec.step_in_direction(VerticalOrientationEdge.NorthWest, -1);
+    vec.step_in_direction(HexDirection.NorthWest, -1, HexLayout.Vertical);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(-1);
-    vec.step_in_direction(VerticalOrientationEdge.SouthWest, -1);
+    vec.step_in_direction(HexDirection.SouthWest, -1, HexLayout.Vertical);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(-1);
-    vec.step_in_direction(VerticalOrientationEdge.East, -1);
+    vec.step_in_direction(HexDirection.East, -1, HexLayout.Vertical);
     expect(vec.q).toBe(0);
     expect(vec.r).toBe(0);
   });
   test('Multiple steps in horizontal directions', () => {
     const vec = new VectorHex(3, -3);
-    vec.step_in_direction(HorizontalOrientationEdge.SouthEast, -2);
+    vec.step_in_direction(HexDirection.SouthEast, -2);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(-1);
-    vec.step_in_direction(HorizontalOrientationEdge.North, 5);
+    vec.step_in_direction(HexDirection.North, 5);
     expect(vec.q).toBe(1);
     expect(vec.r).toBe(4);
   });
   test('Multiple steps in vertical directions', () => {
-    const vec = new VectorHex(3, -3);
-    vec.step_in_direction(VerticalOrientationEdge.NorthWest, 2);
-    expect(vec.q).toBe(3);
+    const vec = new VectorHex(2, 2);
+    vec.step_in_direction(HexDirection.West, 2, HexLayout.Vertical);
+    expect(vec.q).toBe(0);
+    expect(vec.r).toBe(4);
+    vec.step_in_direction(HexDirection.SouthEast, 5, HexLayout.Vertical);
+    expect(vec.q).toBe(0);
     expect(vec.r).toBe(-1);
-    vec.step_in_direction(VerticalOrientationEdge.SouthWest, 5);
-    expect(vec.q).toBe(-2);
+    vec.step_in_direction(HexDirection.NorthEast, 4, HexLayout.Vertical);
+    expect(vec.q).toBe(4);
     expect(vec.r).toBe(-1);
   });
 });
 
 describe('Rotations', () => {
-  test('60 degree rotation', () => {
+  test('Wedge rotation', () => {
+    expect(() => new VectorHex(0, 0).wedge_rotation(0.5)).toThrow(
+      'must be integral',
+    );
     let vec;
-    (vec = new VectorHex(3, -2)).sector_rotation(0);
+    (vec = new VectorHex(3, -2)).wedge_rotation(0);
     expect(vec.q).toBe(3);
     expect(vec.r).toBe(-2);
-    (vec = new VectorHex(3, -2)).sector_rotation(1);
+    (vec = new VectorHex(3, -2)).wedge_rotation(1);
     expect(vec.q).toBe(2);
     expect(vec.r).toBe(1);
-    (vec = new VectorHex(3, -2)).sector_rotation(2);
+    (vec = new VectorHex(3, -2)).wedge_rotation(2);
     expect(vec.q).toBe(-1);
     expect(vec.r).toBe(3);
-    (vec = new VectorHex(3, -2)).sector_rotation(-3);
+    (vec = new VectorHex(3, -2)).wedge_rotation(-3);
     expect(vec.q).toBe(-3);
     expect(vec.r).toBe(2);
-    (vec = new VectorHex(3, -2)).sector_rotation(7);
+    (vec = new VectorHex(3, -2)).wedge_rotation(7);
     expect(vec.q).toBe(2);
     expect(vec.r).toBe(1);
   });
   test('Step rotation', () => {
+    expect(() => new VectorHex(0, 0).step_rotation(0.5)).toThrow(
+      'must be integral',
+    );
     let vec;
     (vec = new VectorHex(3, -2)).step_rotation(0);
     expect(vec.q).toBe(3);
--- src/math/VectorHex.ts
+++ src/math/VectorHex.ts
@@ -1,33 +1,108 @@
 import { Vector2D, Vector2DLike } from './Vector2D';
 
-const sqrt3 = Math.sqrt(3);
 const sqrt3over2 = 0.5 * Math.sqrt(3);
 
 /**
- * @property {Horizontal} - Hexes have horizontal top and bottom edges and the q-axis is aligned with x-axis.
- * @property {Vertical} - Hexes have vertical side edges and the r-axis is aligned with y-axis.
+ * @property {Horizontal} - Hexes have horizontal top and bottom edges and the r-axis is aligned with the y-axis.
+ * @property {Vertical} - Hexes have vertical side edges and the q-axis is aligned with the x-axis.
  */
-export enum HexGridOrientation {
+export enum HexLayout {
   Horizontal,
   Vertical,
 }
 
-export enum HorizontalOrientationEdge {
-  North = 0,
-  NorthWest = 1,
-  SouthWest = 2,
-  South = 3,
-  SouthEast = 4,
-  NorthEast = 5,
+export enum HexDirection {
+  East,
+  NorthEast,
+  North,
+  NorthWest,
+  West,
+  SouthWest,
+  South,
+  SouthEast,
 }
 
-export enum VerticalOrientationEdge {
-  NorthWest = 0,
-  West = 1,
-  SouthWest = 2,
-  SouthEast = 3,
-  East = 4,
-  NorthEast = 5,
+// Currently not needed
+/*function horizontal_direction_to_vertical_direction(
+  direction: HexDirection,
+): HexDirection {
+  switch (direction) {
+    case HexDirection.NorthEast:
+      return HexDirection.NorthEast;
+    case HexDirection.North:
+      return HexDirection.NorthWest;
+    case HexDirection.NorthWest:
+      return HexDirection.West;
+    case HexDirection.SouthWest:
+      return HexDirection.SouthWest;
+    case HexDirection.South:
+      return HexDirection.SouthEast;
+    case HexDirection.SouthEast:
+      return HexDirection.East;
+    default:
+      throw new Error('Invalid direction for horizontal layout.');
+  }
+}*/
+
+function vertical_direction_to_horizontal_direction(
+  direction: HexDirection,
+): HexDirection {
+  switch (direction) {
+    case HexDirection.East:
+      return HexDirection.SouthEast;
+    case HexDirection.NorthEast:
+      return HexDirection.NorthEast;
+    case HexDirection.NorthWest:
+      return HexDirection.North;
+    case HexDirection.West:
+      return HexDirection.NorthWest;
+    case HexDirection.SouthWest:
+      return HexDirection.SouthWest;
+    case HexDirection.SouthEast:
+      return HexDirection.South;
+    default:
+      throw new Error('Invalid direction for vertical layout.');
+  }
+}
+
+function horizontal_wedge_to_vertical_wedge(
+  direction: HexDirection,
+): HexDirection {
+  switch (direction) {
+    case HexDirection.East:
+      return HexDirection.NorthEast;
+    case HexDirection.NorthEast:
+      return HexDirection.North;
+    case HexDirection.NorthWest:
+      return HexDirection.NorthWest;
+    case HexDirection.West:
+      return HexDirection.SouthWest;
+    case HexDirection.SouthWest:
+      return HexDirection.South;
+    case HexDirection.SouthEast:
+      return HexDirection.SouthEast;
+    default:
+      throw new Error('Invalid direction for horizontal wedge.');
+  }
+}
+
+function wedge_edge_direction(wedge: HexDirection): HexDirection {
+  switch (wedge) {
+    case HexDirection.SouthEast:
+      return HexDirection.NorthEast;
+    case HexDirection.East:
+      return HexDirection.North;
+    case HexDirection.NorthEast:
+      return HexDirection.NorthWest;
+    case HexDirection.NorthWest:
+      return HexDirection.SouthWest;
+    case HexDirection.West:
+      return HexDirection.South;
+    case HexDirection.SouthWest:
+      return HexDirection.SouthEast;
+    default:
+      throw new Error('Invalid horizontal wedge.');
+  }
 }
 
 /**
@@ -74,27 +149,41 @@ export class VectorHex {
   }
 
   /**
-   * @param {Vector2DLike} vec2d - Any object with a x- and y-coordinate.
+   * @param {Vector2DLike} vec - Any object with a x- and y-coordinate.
    * @returns {VectorHex} - A vector pointing to the hex identified by (q=x, r=y).
    * @throws if an invalid hex grid orientation is supplied.
    */
   public static from_vector2D(
-    vec2d: Vector2DLike,
-    orientation: HexGridOrientation = HexGridOrientation.Horizontal,
+    vec: Vector2DLike,
+    orientation: HexLayout = HexLayout.Horizontal,
     size = 1,
   ): VectorHex {
-    if (
-      orientation !== HexGridOrientation.Horizontal &&
-      orientation !== HexGridOrientation.Vertical
-    ) {
-      throw new Error('Invalid hex grid orientation.');
+    let vec_horizontal = new Vector2D(vec.x / size, vec.y / size);
+    if (orientation === HexLayout.Vertical) {
+      vec_horizontal = VectorHex.into_horizontal(vec_horizontal);
     }
-    if (orientation === HexGridOrientation.Vertical) {
-      vec2d = VectorHex.into_horizontal(vec2d);
+    const fractional_q = vec_horizontal.x / sqrt3over2;
+    const fractional_r = vec_horizontal.y - 0.5 * fractional_q;
+    return VectorHex.from_fractional_coordinates(fractional_q, fractional_r);
+  }
+
+  private static from_fractional_coordinates(
+    fractional_q: number,
+    fractional_r: number,
+  ): VectorHex {
+    const fractional_s = -fractional_q - fractional_r;
+    let q = Math.round(fractional_q) >> 0;
+    let r = Math.round(fractional_r) >> 0;
+    const s = Math.round(fractional_s) >> 0;
+    const delta_q = Math.abs(fractional_q - q);
+    const delta_r = Math.abs(fractional_r - r);
+    const delta_s = Math.abs(fractional_s - s);
+    if (delta_q > delta_r && delta_q > delta_s) {
+      q = -r - s;
+    } else if (delta_r > delta_s) {
+      r = -q - s;
     }
-    const q = vec2d.x / sqrt3over2;
-    const r = vec2d.y - 0.5 * q;
-    return new VectorHex(Math.trunc(q / size) >> 0, Math.trunc(r / size) >> 0);
+    return new VectorHex(q, r);
   }
 
   /**
@@ -105,32 +194,26 @@ export class VectorHex {
   }
 
   /**
-   * @param {HexGridOrientation} orientation - Determines the hex grid axis alignment.
+   * @param {HexLayout} layout - Determines the hex grid axis alignment.
    * @param {number=1} [size=1] - The distance between two adjacent hexes.
    * @returns {Vector2D} - A vector pointing from the origin to the center of the hex.
    * @throws if the size is not positive or an invalid orientation is supplied.
    */
   public into_vector2d(
-    orientation: HexGridOrientation = HexGridOrientation.Horizontal,
+    layout: HexLayout = HexLayout.Horizontal,
     size: number = 1,
   ): Vector2D {
     if (size <= 0) {
       throw new Error('Hex size must be positive.');
     }
-    if (
-      orientation !== HexGridOrientation.Horizontal &&
-      orientation !== HexGridOrientation.Vertical
-    ) {
-      throw new Error('Invalid hex grid orientation.');
-    }
-    const horizontal_vec2d = new Vector2D(
+    let vec2d = new Vector2D(
       size * sqrt3over2 * this.q,
-      size * (this.r + 0.5 * this.q),
+      size * (0.5 * this.q + this.r),
     );
-    if (orientation === HexGridOrientation.Horizontal) {
-      return horizontal_vec2d;
+    if (layout === HexLayout.Vertical) {
+      vec2d = VectorHex.into_vertical(vec2d);
     }
-    return VectorHex.into_vertical(horizontal_vec2d);
+    return vec2d;
   }
 
   /**
@@ -183,20 +266,26 @@ export class VectorHex {
   }
 
   /**
-   * Which sector does the vector point to?
-   * @returns {number} - An index >= 0 and < 6, identifying the sector. Indices start at 0 indicating the east sector (horizontal orientation) or the north-east sector (vertical orientation).
+   * Which wedge does the hex belong in?
+   * @returns {HexDirection} - The direction of the wedge.
    */
-  public sector(): number {
+  public wedge(layout: HexLayout = HexLayout.Horizontal): HexDirection {
+    let hex_wedge;
     if (
       Math.abs(this.q) >= Math.abs(this.r) &&
       Math.abs(this.q) > Math.abs(this.s())
     ) {
-      return this.q >= 0 ? 0 : 3;
+      hex_wedge = this.q >= 0 ? HexDirection.East : HexDirection.West;
     } else if (Math.abs(this.r) >= Math.abs(this.s())) {
-      return this.r > 0 ? 2 : 5;
+      hex_wedge = this.r > 0 ? HexDirection.NorthWest : HexDirection.SouthEast;
     } else {
-      return this.s() < 0 ? 1 : 4;
+      hex_wedge =
+        this.s() > 0 ? HexDirection.SouthWest : HexDirection.NorthEast;
     }
+    if (layout === HexLayout.Vertical) {
+      hex_wedge = horizontal_wedge_to_vertical_wedge(hex_wedge);
+    }
+    return hex_wedge;
   }
 
   /**
@@ -205,29 +294,40 @@ export class VectorHex {
    * @param steps
    * @throws if an invalid axis number is found.
    */
-  public step_in_direction(direction: number, steps: number = 1): VectorHex {
+  public step_in_direction(
+    direction: HexDirection,
+    steps: number = 1,
+    layout: HexLayout = HexLayout.Horizontal,
+  ): VectorHex {
     if (!Number.isInteger(steps)) {
-      throw new Error('Number of steps must be an integer.');
+      throw new Error('Number of steps must be integral.');
     }
-    if (direction < 0 || direction >= 6) {
-      throw new Error('Invalid direction, must be a positive integer < 6.');
-    }
-    if (direction > 2) {
-      steps *= -1;
-      direction -= 3;
+    if (layout === HexLayout.Vertical) {
+      direction = vertical_direction_to_horizontal_direction(direction);
     }
     switch (direction) {
-      // North
-      case 0:
+      case HexDirection.North:
         this.r += steps;
         break;
-      case 1: // NorthWest
+      case HexDirection.South:
+        this.r -= steps;
+        break;
+      case HexDirection.NorthWest:
         this.q -= steps;
         this.r += steps;
         break;
-      case 2: // SouthWest
+      case HexDirection.SouthEast:
+        this.q += steps;
+        this.r -= steps;
+        break;
+      case HexDirection.NorthEast:
+        this.q += steps;
+        break;
+      case HexDirection.SouthWest:
         this.q -= steps;
         break;
+      default:
+        throw new Error('Invalid horizontal direction');
     }
     return this;
   }
@@ -237,18 +337,19 @@ export class VectorHex {
    * @param {number} [n] - The number of 60 degree rotations (can be negative).
    * @throws if the number of sector rotations is not integral.
    */
-  public sector_rotation(n: number) {
+  public wedge_rotation(n: number): VectorHex {
     if (!Number.isInteger(n)) {
-      throw new Error('Number of sectors must be integral.');
+      throw new Error('Number of wedges must be integral.');
     }
     n %= 6;
-    if (n === 0) return;
+    if (n === 0) return this;
     n = n < 0 ? n + 6 : n;
     const sign_shift = 1 - (n % 2) * 2;
     const coordinate_shift = n % 3;
     const coordinates: number[] = [this.q, this.r, this.s()];
     this.q = sign_shift * coordinates[coordinate_shift];
     this.r = sign_shift * coordinates[(1 + coordinate_shift) % 3];
+    return this;
   }
 
   /**
@@ -268,32 +369,30 @@ export class VectorHex {
     n = n < 0 ? n + circumference : n; // 0 <= n < circumference
     n -= this.rotate_to_corner(n);
     const sector_rotations = Math.trunc(n / hexes_per_sector);
-    this.sector_rotation(sector_rotations);
+    this.wedge_rotation(sector_rotations);
     n -= sector_rotations * hexes_per_sector;
-    const sector = this.sector();
-    const direction = sector;
-    this.step_in_direction(direction, n);
+    this.step_in_direction(wedge_edge_direction(this.wedge()), n);
   }
 
   private rotate_to_corner(max_steps: number): number {
     // Rotate hex wise until a corner is reached
-    const current_sector: number = this.sector();
+    const direction = wedge_edge_direction(this.wedge());
     let steps_taken: number = 0;
-    switch (current_sector) {
-      case HorizontalOrientationEdge.North:
-      case HorizontalOrientationEdge.South:
-        steps_taken = Math.min(Math.abs(this.r), max_steps);
-        this.step_in_direction(current_sector, steps_taken);
-        break;
-      case HorizontalOrientationEdge.NorthWest:
-      case HorizontalOrientationEdge.SouthEast:
-        steps_taken = Math.min(Math.abs(this.q), max_steps);
-        this.step_in_direction(current_sector, steps_taken);
-        break;
-      case HorizontalOrientationEdge.SouthWest:
-      case HorizontalOrientationEdge.NorthEast:
+    switch (direction) {
+      case HexDirection.NorthEast:
+      case HexDirection.SouthWest:
         steps_taken = Math.min(Math.abs(this.s()), max_steps);
-        this.step_in_direction(current_sector, steps_taken);
+        this.step_in_direction(direction, steps_taken);
+        break;
+      case HexDirection.North:
+      case HexDirection.South:
+        steps_taken = Math.min(Math.abs(this.r), max_steps);
+        this.step_in_direction(direction, steps_taken);
+        break;
+      case HexDirection.NorthWest:
+      case HexDirection.SouthEast:
+        steps_taken = Math.min(Math.abs(this.q), max_steps);
+        this.step_in_direction(direction, steps_taken);
         break;
     }
     return steps_taken;
