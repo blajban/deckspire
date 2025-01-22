b95a0686417c58ac6e274d2cf4dc05a03ee4c465
DrK-ode <mailtokaj@gmail.com>
2025-01-22 02:02:29 +0100

    Removed unused import.

--- src/engine/components/CompHex.ts
+++ src/engine/components/CompHex.ts
@@ -1,6 +1,5 @@
 import { HexCoordinates } from '../../math/hexgrid/HexVectors';
 import { Component } from '../core/Component';
-import { Entity } from '../core/Entity';
 
 /**
  * Represents a hex in a hex grid.
