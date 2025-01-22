59d624d04be05d2cf69fdb2b633ba52a5a75b01a
Erik Sjöberg <sjoerik@gmail.com>
2025-01-22 13:24:36 +0100

    Added parent-child components and example in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,17 @@ import { CompRotation } from './engine/components/CompRotation';
 import EntityStore from './engine/core/EntityStore';
 import { World } from './engine/core/World';
 import { loadJsonFile } from './engine/util/file';
+import { CompParent } from './engine/components/CompParent';
+import { CompChild } from './engine/components/CompChild';
+import ParentChildExampleSystem from './systems/ParentChildExampleSystem';
 
 class MainScene extends Phaser.Scene {
   private entityStore = new EntityStore();
   private componentStore = new ComponentStore();
   private world =  new World(this.entityStore, this.componentStore);
+
+  private parentChildExampleSystem = new ParentChildExampleSystem();
+
   constructor() {
     super('MainScene');
 
@@ -20,6 +26,10 @@ class MainScene extends Phaser.Scene {
 
     this.world.registerComponent(CompPosition);
     this.world.registerComponent(CompRotation);
+    this.world.registerComponent(CompParent);
+    this.world.registerComponent(CompChild);
+
+    
 
     loadJsonFile('/world.json')
     .then((data) => {
@@ -59,17 +69,43 @@ class MainScene extends Phaser.Scene {
     
     //this.world.deserialize(jsonData);
 
-    
+    const parent = this.world.newEntity();
+  
+    const child1 = this.world.newEntity();
+    this.world.addComponent(child1, new CompChild(parent));
+    const child2 = this.world.newEntity();
+    this.world.addComponent(child2, new CompChild(parent));
+    const child3 = this.world.newEntity();
+    this.world.addComponent(child3, new CompChild(parent));
+    const child4 = this.world.newEntity();
+    this.world.addComponent(child4, new CompChild(parent));
 
     
-    
+    this.world.addComponent(parent, new CompParent([
+      child1,
+      child2,
+      child3,
+      child4
+    ]));
+
 
     
     
   }
 
   update(time: number, delta: number) {
-    console.log(this.world.getEntitiesWithArchetype(CompPosition));
+    //console.log(this.world.getEntitiesWithArchetype(CompPosition));
+
+    this.parentChildExampleSystem.update(this.world, time, delta);
+
+    const children = this.world.getEntitiesWithComponent(CompChild);
+
+    for (const child of children) {
+      const childComp = this.world.getComponent(child, CompChild);
+
+      console.log(`Child #${child} has parent ${childComp?.parent}`);
+    }
+
   }
 }
 
---
+++ src/systems/ParentChildExampleSystem.ts
@@ -0,0 +1,17 @@
+import { CompParent } from '../engine/components/CompParent';
+import { System } from '../engine/core/System';
+import { World } from '../engine/core/World';
+
+export default class ParentChildExampleSystem extends System {
+  update(world: World, time: number, delta: number) {
+    const parents = world.getEntitiesWithComponent(CompParent);
+    
+    for (const parent of parents) {
+      const parentComp = world.getComponent(parent, CompParent);
+
+      parentComp?.children.forEach((child) => {
+        console.log(`Child #${child}`);
+      });
+    }
+  }
+}
