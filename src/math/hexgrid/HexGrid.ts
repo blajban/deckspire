a8d12edeedccfe65aeed93073c60de745b6ad02b
DrK-ode <mailtokaj@gmail.com>
2025-01-22 02:02:56 +0100

    Added documentation comments.

--- src/math/hexgrid/HexGrid.ts
+++ src/math/hexgrid/HexGrid.ts
@@ -2,7 +2,7 @@ import { HexDistance, HexDirection, HexCoordinates } from './HexVectors';
 
 export type HexGridGuard = (hex: HexCoordinates) => boolean;
 
-export default class HexGrid{
+export default class HexGrid {
   /**
    * Represents a hex grid.
    * @param grid_guard - A function that returns true if a hex is part of the grid. Default is an infinite grid.
@@ -11,10 +11,21 @@ export default class HexGrid{
     public grid_guard: HexGridGuard = (hex: HexCoordinates) => true,
   ) {}
 
+  /**
+   * Checks whether a hex is in the grid.
+   * @param hex
+   * @returns true if the hex coordinates is part of the grid.
+   */
   public is_hex_in_grid(hex: HexCoordinates): boolean {
     return this.grid_guard(hex);
   }
 
+  /**
+   * Returns the hexes within a manhattan radius.
+   * @param center - the hex from which to measure the radius.
+   * @param range - the manhattan radius.
+   * @returns an array of hexes within the manhattan radius.
+   */
   public hexes_within_manhattan_radius(
     center: HexCoordinates,
     range: number,
@@ -35,6 +46,12 @@ export default class HexGrid{
     return results;
   }
 
+  /**
+   * Return all hexes reachable within a number of steps including the starting hex.
+   * @param center - the starting coordinates.
+   * @param steps - How many steps to take.
+   * @returns an array of hexes within the step limit.
+   */
   public hexes_within_steps(
     center: HexCoordinates,
     steps: number,
