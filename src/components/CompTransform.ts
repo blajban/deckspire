1c31e013db38dcc6d5733eee8587428b028b2984
DrK-ode <mailtokaj@gmail.com>
2025-01-24 14:18:08 +0100

    Uses shorthand notation in ctor.

--- src/components/CompTransform.ts
+++ src/components/CompTransform.ts
@@ -2,16 +2,16 @@ import Vector2D from '../math/Vector2D';
 import Component from '../engine/core/Component';
 
 export default class CompTransform extends Component {
-  public position: Vector2D;
-  public rotation: number;
-  public scale: Vector2D;
-
   /**
    * @param {Vector2D} position - Center of the object.
    * @param {number} [rotation = 0] - in radians.
    * @param {Vector2D} [scale = (1,1)] - Scales x- and y-size separately
    */
-  constructor(position: Vector2D, rotation: number = 0, scale: Vector2D = new Vector2D(1, 1)) {
+  constructor(
+    public position: Vector2D,
+    public rotation: number = 0,
+    public scale: Vector2D = new Vector2D(1, 1),
+  ) {
     super();
     this.position = position;
     this.rotation = rotation;
