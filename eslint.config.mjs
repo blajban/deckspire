850c10df0b3dba250858f72efc2b13f97a5744f6
DrK-ode <mailtokaj@gmail.com>
2025-01-27 15:08:56 +0100

    Linting with type checks.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,102 +1,110 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
 export default [
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ),
+  ...tseslint.configs.recommendedTypeChecked,
   {
-    files: ['src/**/*.ts', '__tests__/**/*.ts'],
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      prettier,
-    },
+  files: ['src/**/*.ts', '__tests__/**/*.ts'],
 
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
-      parser: tsParser,
-    },
-
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['warn', 'single'],
-      indent: ['warn', 2, { SwitchCase: 1 }],
-      eqeqeq: 'warn',
-      curly: ['error', 'all'],
-      'prefer-const': 'warn',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: ['default'],
-          format: ['camelCase'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'allow',
-        },
-        {
-          selector: ['typeLike', 'import'],
-          format: ['PascalCase'],
-          leadingUnderscore: 'forbid',
-          trailingUnderscore: 'forbid',
-        },
-        {
-          selector: ['class', 'interface'],
-          format: ['PascalCase'],
-          leadingUnderscore: 'forbid',
-        },
-        {
-          selector: ['classMethod', 'function'],
-          format: ['camelCase'],
-          leadingUnderscore: 'forbid',
-        },
-        {
-          selector: ['classMethod'],
-          modifiers: ['private'],
-          format: ['camelCase'],
-          leadingUnderscore: 'require',
-        },
-        {
-          selector: [
-            'variable',
-            'classProperty',
-            'accessor',
-            'parameter',
-            'parameterProperty',
-          ],
-          format: ['snake_case', 'UPPER_CASE'],
-          leadingUnderscore: 'forbid',
-        },
-        {
-          selector: ['variable', 'classProperty'],
-          modifiers: ['private'],
-          format: ['snake_case', 'UPPER_CASE'],
-          leadingUnderscore: 'require',
-        },
-      ],
-      'prettier/prettier': 'warn',
-    },
+  plugins: {
+    tsplugin: tseslint.plugin,
+    prettier,
   },
-];
+
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+    },
+    parser: tseslint.parser,
+    parserOptions: {
+      project: ['tsconfig.json'],
+    }
+  },
+
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['warn', 'single'],
+    indent: ['warn', 2, { SwitchCase: 1 }],
+    eqeqeq: 'warn',
+    curly: ['error', 'all'],
+    'prefer-const': 'warn',
+    'no-unused-vars': 'off',
+    'tsplugin/no-unused-vars': ['warn'],
+    'tsplugin/explicit-function-return-type': 'error',
+    'tsplugin/no-explicit-any': 'error',
+    'tsplugin/naming-convention': [
+      'error',
+      {
+        selector: ['default'],
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'import',
+        format: [],
+      },
+      {
+        selector: ['typeLike'],
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: ['class', 'interface'],
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+      },
+      {
+        selector: ['classMethod', 'function'],
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+      },
+      {
+        selector: ['classMethod'],
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: [
+          'variable',
+          'classProperty',
+          'accessor',
+          'parameter',
+          'parameterProperty',
+        ],
+        format: ['snake_case'],
+        leadingUnderscore: 'forbid',
+      },
+      {
+        selector: ['variable', 'classProperty', 'parameterProperty'],
+        modifiers: ['private'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: [
+          'variable',
+          'classProperty',
+          'accessor',
+          'parameter',
+          'parameterProperty',
+        ],
+        types: ['boolean'],
+        format: ['snake_case'],
+        leadingUnderscore: 'forbid',
+        prefix: ['is_', 'should_', 'has_', 'can_', 'did_', 'will_'],
+      },
+      {
+        selector: ['variable', 'classProperty', 'parameterProperty'],
+        modifiers: ['private'],
+        types: ['boolean'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+        prefix: ['is_', 'should_', 'has_', 'can_', 'did_', 'will_'],
+      },
+    ],
+    'prettier/prettier': 'warn',
+  },
+}];
