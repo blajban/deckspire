75cb33c10d4bae68e9b3a94671fd70abd53edff0
DrK-ode <mailtokaj@gmail.com>
2025-01-27 13:52:51 +0100

    Fixed linting configuration problems and added formatting rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import stylisticJs from '@stylistic/eslint-plugin-js';
 import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
@@ -19,6 +18,7 @@ const compat = new FlatCompat({
 export default [
   ...compat.extends(
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ),
@@ -26,7 +26,6 @@ export default [
     files: ['src/**/*.ts', '__tests__/**/*.ts'],
     plugins: {
       '@typescript-eslint': typescriptEslint,
-      '@stylistic/js': stylisticJs,
       prettier,
     },
 
@@ -41,14 +40,62 @@ export default [
     rules: {
       semi: ['error', 'always'],
       quotes: ['warn', 'single'],
-      indent: ['warn', 2, { switchCase: 1 }],
+      indent: ['warn', 2, { SwitchCase: 1 }],
       eqeqeq: 'warn',
       curly: ['error', 'all'],
       'prefer-const': 'warn',
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['warn'],
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['default'],
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: ['typeLike', 'import'],
+          format: ['PascalCase'],
+          leadingUnderscore: 'forbid',
+          trailingUnderscore: 'forbid',
+        },
+        {
+          selector: ['class', 'interface'],
+          format: ['PascalCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: ['classMethod', 'function'],
+          format: ['camelCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: ['classMethod'],
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: [
+            'variable',
+            'classProperty',
+            'accessor',
+            'parameter',
+            'parameterProperty',
+          ],
+          format: ['snake_case', 'UPPER_CASE'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: ['variable', 'classProperty'],
+          modifiers: ['private'],
+          format: ['snake_case', 'UPPER_CASE'],
+          leadingUnderscore: 'require',
+        },
+      ],
       'prettier/prettier': 'warn',
     },
   },
