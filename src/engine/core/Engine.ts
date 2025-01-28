c41008552793c679131998c0711084b79b37d7e7
Erik Sjöberg <sjoerik@gmail.com>
2025-01-28 23:28:36 +0100

    Removed unnecessary vars

--- src/engine/core/Engine.ts
+++ src/engine/core/Engine.ts
@@ -6,26 +6,10 @@ export default class Engine extends Phaser.Scene {
   private _width: number;
   private _height: number;
   private _phaser_context: Phaser.Game;
-  private _registered_scenes: Map<string, Scene> = new Map();
-  private _active_scenes: Map<string, Scene> = new Map();
   private _ready_resolver: (() => void) | null = null;
   private _ready_promise: Promise<void>;
 
   private _scene_manager: SceneManager = new SceneManager(this);
-
-  // [OK] Make sure the phaser scene is ready before doing things in the Scene
-  // Assets
-  // Systems
-  // Global objects
-  // [OK] Scene transitions/fade in/fade out (handled manually for now)
-  // - Stop scene
-  // - Pause scene
-  // [OK] Add scene manager
-  // [OK] Pass scene instead of world to systems
-  // Serialization
-  // Testing
-  // Comments
-  // Remove scene entirely?
 
   constructor(width: number, height: number) {
     super('Engine');
