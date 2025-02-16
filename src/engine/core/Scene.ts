7dd6e1edae134dfbd5969aa55bad6127b43ad6fe
DrK-ode <mailtokaj@gmail.com>
2025-02-16 23:19:22 +0100

    Made Scene abstract.

--- src/engine/core/Scene.ts
+++ src/engine/core/Scene.ts
@@ -1,6 +1,6 @@
 import { GameContext } from './GameContext';
 
 export default abstract class Scene {
-  buildScene(_context: GameContext): void {}
-  destroyScene(_context: GameContext): void {}
+  abstract buildScene(context: GameContext): void;
+  abstract destroyScene(context: GameContext): void;
 }
